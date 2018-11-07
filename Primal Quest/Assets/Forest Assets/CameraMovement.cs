527b649d18778e0b29b4bb84e14388961b70cabd
AaronMasson <33638411+AaronMasson@users.noreply.github.com>
2018-11-06 20:07:40 -0500

    Started Playing with the Forest
    Started the terrain and layout of the levels objective.

---
+++ Primal Quest/Assets/Forest Assets/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speedLeftRight = 2f;
+    public float speedUpDown = 2f;
+    private float leftRight = 0f;
+    private float upDown = 0f;
+    GameObject character;
+    // Use this for initialization
+    void Start()
+    {
+        //Because this script is not attached to our player we have to find
+        //that object by using the code below.
+        character = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //This will make sure that the cursor cannot leave the screen
+        //if you want the cursor to leave the screen press escape
+        //Given by Joshua
+        Cursor.lockState = CursorLockMode.Locked;
+        leftRight += speedLeftRight * Input.GetAxis("Mouse X");
+        upDown += speedUpDown * Input.GetAxis("Mouse Y");
+
+        //This will move the camera
+        transform.eulerAngles = new Vector3(upDown * -1, leftRight, 0f);
+        //This will roate the player so that the player and the camera are
+        //facing the right direction.
+        character.transform.eulerAngles = new Vector3(0f, leftRight, 0f);
+    }
+}
---
+++ Primal Quest/Assets/Forest Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 20f;
+    public float jumpForce = 10f;
+    public float gravity = 30f;
+    public Vector3 move = Vector3.zero;
+    CharacterController controller;
+    // Use this for initialization
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+
+    }
+
+    void movePlayer()
+    {
+        if (controller.isGrounded)
+        {
+            move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            move = transform.TransformDirection(move);
+            move *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                move.y = jumpForce;
+            }
+        }
+
+        move.y -= gravity * Time.deltaTime;
+
+        controller.Move(move * Time.deltaTime);
+    }
+}
